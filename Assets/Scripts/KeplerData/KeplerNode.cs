2414b3b2a5aeaa22e079252012ffbb01a661f1ae
Daniel Plemmons <dpx109@gmail.com>
2016-03-14 01:17:36 -0700

    Move StellarCoordinates into their own file.

--- Assets/Scripts/KeplerData/KeplerNode.cs
+++ Assets/Scripts/KeplerData/KeplerNode.cs
@@ -3,30 +3,6 @@ using VRViz.Core;
 using System.Collections;
 
 namespace KeplerData {
-
-  public struct StellarCoordinates {
-    public float rightAscention;
-    public float declination;
-    public float distance;
-
-    public StellarCoordinates(float ra, float dec, float dist) {
-      rightAscention = ra;
-      declination = dec;
-      distance = dist;
-    }
-
-    public Quaternion ToQuaternion() {
-      // declination is flipped to fit unity's axis
-      return Quaternion.Euler(-declination, rightAscention, 0.0f);
-    }
-
-    public Vector3 ToPosition(Mapping<float, float> distanceMapping) {
-      if (distanceMapping == null)
-        throw new System.NullReferenceException("Null distance mapping provided.");
-      return ToQuaternion() * Vector3.forward * distanceMapping.MapValue(distance);
-    }
-  }
-
   public class KeplerNode : TimeboxedNode {
     public readonly string Name;
     public readonly StellarCoordinates Position;
@@ -38,6 +14,5 @@ namespace KeplerData {
         Position = position;
         DiscoveringFaciltiy = discoveringFaciltiy;
     }
-    
   }
 }
---
+++ Assets/Scripts/KeplerData/StellarCoordinates.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using VRViz.Core;
+using VRViz.Unity;
+
+namespace KeplerData {
+  public struct StellarCoordinates :
+      IEquatable<StellarCoordinates>,
+      IComparable<StellarCoordinates> {
+    public float rightAscention;
+    public float declination;
+    public float distance;
+
+    public StellarCoordinates(float ra, float dec, float dist) {
+      rightAscention = ra;
+      declination = dec;
+      distance = dist;
+    }
+
+    public bool Equals(StellarCoordinates coords2) {
+      return rightAscention == coords2.rightAscention && declination == coords2.declination && distance == coords2.distance;
+    }
+
+    public int CompareTo(StellarCoordinates coords2) {
+      float thisManhattan = rightAscention + declination + distance;
+      float coords2Manhattan = coords2.rightAscention + coords2.declination + coords2.distance;
+
+      if (thisManhattan > coords2Manhattan)
+        return 1;
+      else if (thisManhattan == coords2Manhattan)
+        return 0;
+      else
+        return -1;
+    }
+  }
+}
